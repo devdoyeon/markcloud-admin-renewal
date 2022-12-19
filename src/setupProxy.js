397e16e79540dfb3f7bbcd61d33c190980cba333
DOYEON-YOU <doyeonyou@naver.com>
2022-12-19 13:40:41 +0900

    modify: main API proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const t = 'http://192.168.0.25:5555/';
+const t = 'http://192.168.0.47:5555/';
 const us = 'http://192.168.0.102:8980/';
 
 module.exports = function (app) {
