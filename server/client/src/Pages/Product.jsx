5d82d164e8073cbd656c6ae654e7c3adc74cb9e8
DOYEON-YOU <doyeonyou@naver.com>
2022-12-28 13:45:20 +0900

    add: service && product Page

---
+++ server/client/src/Pages/Product.jsx
@@ -0,0 +1,17 @@
+import SideBar from "../Components/SideBar";
+
+const Product = () => {
+  return (
+    <div className="container">
+      <SideBar/>
+      <div className="content-wrap product">
+        <div className="topBar">
+          <h2>MERCHANT</h2>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
